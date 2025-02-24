9c5fe94949b01f231eaeb13e64096eda38784e2b
Irvine Fleith <irvine.fleith@atm-consulting.fr>
2025-02-24 16:24:35 +0100

    fix(modal): slash char was not escaped

--- class/advancedProductSearch.class.php
+++ class/advancedProductSearch.class.php
@@ -583,7 +583,6 @@ class AdvancedProductSearch
 			.$db->plimit($this->search['limit'] + 1, $this->search['offset']);
 
 		if($this->displayResults) {
-
 			$querySearchRes = $db->query($this->searchSqlList);
 
 			if ($querySearchRes) {
@@ -1135,6 +1134,8 @@ class AdvancedProductSearch
 		$needle = str_replace('i', '[íîìï]', $needle);
 		$needle = str_replace('o', '[óôòøõö]', $needle);
 		$needle = str_replace('u', '[úûùü]', $needle);
+
+		$needle = preg_quote($needle, '/');
 
 		return preg_replace("/($needle)/iu", sprintf('<span style="background-color: %s; color:%s;">$1</span>', $backgroundColor, $color), $haystack);
 	}
